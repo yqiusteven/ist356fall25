f5d541fd212d6a856cec780486e38799b986c085
auramnar <auramnar@g.syr.edu>
2025-09-24 11:38:08 -0400

    streamlit

--- 2-ui/2-hello.py
+++ 2-ui/2-hello.py
@@ -1,7 +1,11 @@
 import streamlit as st
 
 st.title("Saying Hello.")
+#name ='test'
+#st.write(f'Name is {name}')
 name = st.text_input("And you are?")
 
 if name:
     st.write(f"Hello, {name}!")
+#else:
+    #st.write("Enter a name")
